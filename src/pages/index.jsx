d17621fdc9cc44e65026beae8a9ab1eed37e6e9d
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2018-12-29 00:08:16 +0530

    Completed hero

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Container from '../components/container';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import Hero from '../components/hero';
 
 export default () => (
   <Container>
     <Navbar />
+    <Hero />
     <Footer />
   </Container>
 );
