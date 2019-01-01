12096b27686a7397565293a4654f7635b404d90a
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2019-01-01 20:03:07 +0530

    Updates

--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'gatsby';
-
-export default () => (
-  <div className="navbar">
-    <Link to="/" className="navbar--logo">RP</Link>
-    <Link to="/blog" className="navbar--link">Blog</Link>
-  </div>
-);
--- src/pages/blog.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Container from '../components/Container'
-
-export default () => (
-  <Container>
-    <p className="hero--header">Coming soon...</p>
-  </Container>
-)
