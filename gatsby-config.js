2a13c16cd968f08a86c1bedc6cfb081701e21b56
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2019-01-03 10:16:33 +0530

    Simplified Google Analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,7 @@ plugins: [
     resolve: `gatsby-plugin-google-analytics`,
     options: {
       trackingId: "UA-131670283-1",
-      head: false,
-      anonymize: true,
-      respectDNT: true,
-      exclude: [],
-      sampleRate: 5,
-      siteSpeedSampleRate: 10,
-      cookieDomain: "ratnasambhav.github.io",
+      head: true,
     },
   },
 ]
