377b4061005b8ca0c7f93c2c5322c048908dfbf0
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2019-01-04 20:01:27 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-09f7b2b8800e0cdf4bd6.js"
+    "url": "webpack-runtime-3e728e1b71315a7da525.js"
   },
   {
     "url": "app-f386d6ae50437afccb2a.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e83d09f9c6bd836e735654ea70f9a2db"
+    "revision": "7d7bc9ce64d27736d3d1d67374e6cdd1"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
