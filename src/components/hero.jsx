9da8c99f2ea0f5d183b5e9357f39f84d6cb96d24
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2019-01-04 11:48:43 +0530

    Added br b/w paragraphs in hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,9 +6,12 @@ export default () => (
     <span className="hero--header">Ratnasmabhav Priyadarshi</span>
     <span className="hero--text">I&apos;m studying Computer Science at VIT Vellore. Right now, I&apos;m also an intern at Amazon in Banglore.
     <br/>
+    <br/>
     I love making things with code and experimenting with different programming languages. I primarly work on web technologies because of its ubiquitous nature. I also like programming in Python, Elixir and C#.
     <br/>
+    <br/>
     I made this website to serve as a blog. Though I haven&apos;t started writing anything yet, I want to write about which I find intresting.
+    <br/>
     <br/>
     Thank you for visiting! Your feedback is always welcomed.</span>
   </div>
